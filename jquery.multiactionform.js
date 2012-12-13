39e68143e1ff7773664348fc33d9bfd8fe1659e0
unknown <Pedro.Montoya@PMONTOYA02.corp.neudesic.net>
2012-12-12 21:56:42 -0800

    first commit

---
+++ jquery.multiactionform.js
@@ -0,0 +1,63 @@
+﻿(function ($) {
+
+    //###############################################################################
+
+    /*  
+    *   jQuery multiActionForm plugin
+    *   Original author: @pedromontoya
+    *   Description:
+    */
+
+    //###############################################################################
+
+    var defaultSettings = {};
+
+    $.fn.multiActionForm = function (settingArgs) {
+        //Initialize plugin settings.
+        var settings = $.extend({}, defaultSettings);
+
+        if (settingArgs) {
+            $.extend(settings, settingArgs);
+        }
+
+        //Iterate over each form in the selected element collection.
+        return this.each(function () {
+            var $formElement = $(this),                        //Wrap form element with jQuery Object.
+                $submitElements = $("[type='submit']", $formElement);
+
+            if ($submitElements && $submitElements.length > 0) {
+                //Keep track of the submit button that was clicked.
+                var clickedButton = null;
+                $submitElements.each(function () {
+                    $(this).click(function () {
+                        clickedButton = this;
+                    });
+                });
+
+                //Register submit handler for multi action form.
+                $formElement.submit(function (e) {
+                    if (clickedButton) {
+                        var action = $(clickedButton).data("submitaction");
+                        if (action) {
+                            this.action = action;
+                            return true;
+                        }
+                    }
+
+                    //Unknown button clicked, prevent default.
+                    return false;
+                });
+            }
+        });
+    };
+
+    //###############################################################################
+
+    //Initialize forms.
+    $(function () {
+        $("[data-multiactionform]").multiActionForm();
+    });
+
+    //###############################################################################
+
+})(jQuery)
